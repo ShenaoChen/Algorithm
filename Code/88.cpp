2d23094759cb30f6c6ec5b681f693dd97988f6ae
ShenaoChen <81500905+ShenaoChen@users.noreply.github.com>
2022-01-16 20:11:45 +0800

    88. 合并两个有序数组

---
+++ Code/88.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> tmp;
+        int i = 0, j = 0;
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j])
+                tmp.push_back(nums1[i++]);
+            else
+                tmp.push_back(nums2[j++]);
+        }
+        while(i < m) tmp.push_back(nums1[i++]);
+        while(j < n) tmp.push_back(nums2[j++]);
+        for(int i = 0; i < m + n; i++)
+            nums1[i] = tmp[i];
+    }
+};
