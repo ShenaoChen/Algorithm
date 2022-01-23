dd7a46e9816101ba02477e1c2f7fd22e79b4742b
ShenaoChen <81500905+ShenaoChen@users.noreply.github.com>
2022-01-23 18:39:00 +0800

    1332. 删除回文子序列

---
+++ Code/1332.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        bool flag = true;
+        for(int i = 0, j = s.size() - 1; i < j; i++, j--){
+            if(s[i] != s[j]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag) return 1;
+        else return 2;
+    }
+};
